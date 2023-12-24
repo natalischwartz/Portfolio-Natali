2aaa44e979ddaf1ac8d2f30135e24790b0bafb51
Natali Schwartz <schwartznatali@gmail.com>
2023-12-24 14:39:54 -0300

    se hicieron secciones de skills y qualification

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -36,10 +36,49 @@ navLink.forEach(link =>{
 })
 
 /*==================== ACCORDION SKILLS ====================*/
+// const skillsContent = document.querySelectorAll(".skills__content");
+// console.log(skillsContent)
+// const skillsHeader = document.querySelectorAll(".skills__header");
+// console.log(skillsHeader)
+// const skillList = document.querySelectorAll(".skills__list")
+
+// skillsContent.forEach(el=>{
+//     el.addEventListener("click",()=>{
+//         skillList.classList.toggle("skills__bground")
+        
+//     })
+// })
+
+
+
+
+
+
+
+
 
 
 /*==================== QUALIFICATION TABS ====================*/
+const tabs = document.querySelectorAll('[data-target]')
+const tabContents = document.querySelectorAll('[data-content]')
 
+
+tabs.forEach(tab =>{
+    tab.addEventListener("click", ()=>{
+        const target = document.querySelector(tab.dataset.target)
+
+
+        tabContents.forEach(tabContent =>{
+            tabContent.classList.remove('qualification__active')
+        })
+        target.classList.add('qualification__active')
+
+        tabs.forEach(tab =>{
+            tab.classList.remove('qualification__active')
+        })
+        tab.classList.add('qualification__active')
+    })
+})
 
 /*==================== SERVICES MODAL ====================*/
 
